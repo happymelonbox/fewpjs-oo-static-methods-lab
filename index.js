7b6eae5a08fd591239951779b514b806bb92d9dd
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-11 23:09:46 +1000

    working on titleize function - trying to work it out to make it return the sentence with each word capitalized unless they equal the exceptions

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string){
+    let split = string.split(' ')
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    split[0].charAt(0).toUpperCase()
+    for (let value of split){
+      for (let val of exceptions){
+        if (value !== val){
+          return value.charAt(0).toUpperCase()
+        }
+      }
+    }
+    return split.join(',')
+  }
 }
